fe7045f55c1c380bd3dbf2ed1256458e573f9131
Carlos Andres De La Rosa <rosacar@afis-mgmt.local>
2019-02-22 13:06:30 +0100

    working with ip addresses example

---
+++ samples/working_with_ip_addresses.py
@@ -0,0 +1,10 @@
+# Python 3 has a std lib
+# module for working with
+# IP addresses:
+
+import ipaddress
+
+
+print(ipaddress.ip_address('192.168.1.2'))
+
+print(ipaddress.ip_address('2001:af3::'))
